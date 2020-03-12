10b6993fcfa75fffadd548ba6288b71dbc12ce22
Dima <directordmh@tut.by>
2020-03-12 17:50:51 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -38,8 +38,25 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let result = txt => {
+        let d = ''
+        for(let i = 0; i < txt.length; i = i + 2) {
+            txt[i] + txt[i + 1] === '11' ? d = d + '-' : d = d + '.'
+        }
+        return d
+    }
+    let arr = []
+    for(let i = 0; i < expr.length; i = i + 10) {
+        arr.push(expr.slice(i, i + 10))
+    }
+    return  arr.map(u => {
+        if (u[0] === '*') return ' '
+        for(let i = 0; i < u.length; i = i + 2) {
+            if (u[i] === '1') return MORSE_TABLE[result(u.slice(i, 10))]
+        }
+    }).join('')
 }
+console.log(decode('00101010100000000010001011101000101110100000111111**********00001011110000111111000010111000101110100000111010'))
 
 module.exports = {
     decode
